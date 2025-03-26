e2d4bb0d93dc9098c0615a6aedda5a85ecb76340
iiPython <ben@iipython.dev>
2025-03-26 16:02:06 -0500

    rearrange for future proofing

--- logic.py
+++ logic.py
@@ -1,9 +1,11 @@
 # Copyright (c) 2025 Benjamin O'Brien
-# Version 3.2
+# Version 3.3
 
 # Modules
 import re
 import operator
+from enum import Enum
+from dataclasses import dataclass
 
 # Initialization
 OPERATORS = {}
@@ -31,83 +33,139 @@ for symbols, data in {
 GROUPINGS = {"(": ")", "[": "]", "{": "}"}
 REGEX = re.compile(rf"((?:~+)?[A-Z]) ?([{''.join(OPERATORS.keys())}])? ?((?:~+)?[A-Z])?")
 
+# Enums
+class ComparisonMode(Enum):
+    RELATIONSHIP = 1
+    VALIDITY     = 2
+
+@dataclass
+class RelationshipResult:
+    equivalent:    bool
+    consistent:    bool
+    contradictory: bool
+
+type ValidityResult = list[bool]
+
 # Exceptions
 class UnknownOperator(Exception):
     pass
 
 # Methods
-def trim(expression: str) -> str:
-    return expression[1:][:-1] if expression[0] in GROUPINGS.keys() else expression
+class Logic:
+    def __init__(self, enable_logging: bool = True) -> None:
+        self.enable_logging = enable_logging
 
-def evaluate(expression: str, variables: dict[str, bool], index: int = 0, enable_logging: bool = True) -> bool:
-    def log(text: str, indent: int = 0) -> None:
-        if enable_logging:
-            print(f"\033[90m{'\t' * (indent + index)}{text}\033[0m")
+    @staticmethod
+    def trim(expression: str) -> str:
+        return expression[1:][:-1] if expression[0] in GROUPINGS.keys() else expression
 
-    # Take expression, and find the top level assets
-    depth, segments, buffer = 0, [], ""
-    for character in expression:
-        match character:
-            case " ":
-                continue
+    def evaluate(self, expression: str, variables: dict[str, bool], index: int = 0) -> bool:
+        def log(text: str, indent: int = 0) -> None:
+            if self.enable_logging:
+                print(f"\033[90m{'\t' * (indent + index)}{text}\033[0m")
 
-            case _ if character in GROUPINGS.keys():
-                depth += 1
-                buffer += character
+        # Take expression, and find the top level assets
+        depth, segments, buffer = 0, [], ""
+        for character in expression:
+            match character:
+                case " ":
+                    continue
 
-            case _ if character in GROUPINGS.values():
-                depth -= 1
-                buffer += character
-                if not depth:
-                    segments.append(buffer)
+                case _ if character in GROUPINGS.keys():
+                    depth += 1
+                    buffer += character
+
+                case _ if character in GROUPINGS.values():
+                    depth -= 1
+                    buffer += character
+                    if not depth:
+                        segments.append(buffer)
+                        buffer = ""
+
+                case _ if not depth and character in OPERATORS:
+                    if buffer:
+                        segments.append(buffer)
+
+                    segments.append(character)
                     buffer = ""
 
-            case _ if not depth and character in OPERATORS:
-                if buffer:
-                    segments.append(buffer)
+                case _:
+                    buffer += character
 
-                segments.append(character)
-                buffer = ""
+        if buffer:
+            segments.append(buffer)
 
-            case _:
-                buffer += character
+        log(f"Input expression --> {expression}")
 
-    if buffer:
-        segments.append(buffer)
+        # Process chains
+        if len(segments) > 1:
+            left, operator, right = segments
+            left_value, right_value = self.evaluate(self.trim(left), variables, index + 1), self.evaluate(self.trim(right), variables, index + 1)
+            log(f"Segments --> {' '.join(segments)}", 1)
+            log(f"Method --> {OPERATORS[operator]['name']}", 2)
 
-    log(f"Input expression --> {expression}")
+            if operator not in OPERATORS:
+                raise UnknownOperator(f"the specified operator ({operator}) is not known!")
 
-    # Process chains
-    if len(segments) > 1:
-        left, operator, right = segments
-        left_value, right_value = evaluate(trim(left), variables, index + 1, enable_logging), evaluate(trim(right), variables, index + 1, enable_logging)
-        log(f"Segments --> {' '.join(segments)}", 1)
-        log(f"Method --> {OPERATORS[operator]['name']}", 2)
+            value = OPERATORS[operator]["comparison"](left_value, right_value)
+            log(f"Result --> {left_value} {operator} {right_value} (\033[3{2 if value else 1}m{value}\033[90m)", 2)
+            return value
 
-        if operator not in OPERATORS:
-            raise UnknownOperator(f"the specified operator ({operator}) is not known!")
+        # Process non-chained values (or anything with a tilde)
+        else:
+            sanitized = segments[0].lstrip("~")
 
-        value = OPERATORS[operator]["comparison"](left_value, right_value)
-        log(f"Result --> {left_value} {operator} {right_value} (\033[3{2 if value else 1}m{value}\033[90m)", 2)
-        return value
+            # Attempt to find value as a variable
+            value = variables.get(sanitized)
+            if value is None:
 
-    # Process non-chained values (or anything with a tilde)
-    else:
-        sanitized = segments[0].lstrip("~")
+                # If it isn't one, assume it's another chain
+                value = self.evaluate(self.trim(sanitized), variables, index + 1)
 
-        # Attempt to find value as a variable
-        value = variables.get(sanitized)
-        if value is None:
+            # Negate the value as many times as needed
+            for _ in range(len(segments[0]) - len(segments[0].lstrip("~"))):
+                value = not value
 
-            # If it isn't one, assume it's another chain
-            value = evaluate(trim(sanitized), variables, index + 1, enable_logging)
+            log(f"Evaluate --> {segments[0]} (\033[3{2 if value else 1}m{value}\033[90m)", 1)
+            return value
 
-        # Negate the value as many times as needed
-        for _ in range(len(segments[0]) - len(segments[0].lstrip("~"))):
-            value = not value
+    def find_possible_values(self, *expressions) -> list[list[bool]]:
+        variables = []
+        for expression in expressions:
+            variables += [c for c in expression if c in string.ascii_letters]
 
-        log(f"Evaluate --> {segments[0]} (\033[3{2 if value else 1}m{value}\033[90m)", 1)
-        return value
+        results = []
+        def loopback(variables: list[str], values: dict[str, bool] = {}) -> None:
+            current_variable = variables[0]
+            for value in [True, False]:
+                if variables[1:]:
+                    loopback(variables[1:], values | {current_variable: value})
+
+                else:
+                    results.append([
+                        self.evaluate(
+                            expression,
+                            variables = values | {current_variable: value}
+                        )
+                        for expression in expressions
+                    ])
+
+        loopback(sorted(set(variables)))
+        return results
+
+    def compare(self, mode: ComparisonMode, *expressions) -> RelationshipResult | ValidityResult:
+        values = self.find_possible_values(*expressions)
+        match mode:
+            case ComparisonMode.RELATIONSHIP:
+                consistent = any([x and y for x, y in values])
+                return RelationshipResult(
+                    all([x == y for x, y in values]),
+                    consistent,
+                    all([x != y for x, y in values]),
+                )
+
+            case ComparisonMode.VALIDITY:
+                raise NotImplementedError
 
 # CLI
 if __name__ == "__main__":
@@ -125,7 +183,7 @@ if __name__ == "__main__":
         start_time = time.time()
 
         print()
-        result = evaluate(
+        result = Logic().evaluate(
             expression,
             variables = variables
         )
@@ -138,45 +196,19 @@ if __name__ == "__main__":
 
     if expressions:
         if "--compare" in options:
-            variables = []
-            for expression in expressions:
-                variables += [c for c in expression if c in string.ascii_letters]
+            result = Logic(enable_logging = False).compare(ComparisonMode.RELATIONSHIP, *expressions)
+            if not isinstance(result, RelationshipResult):
+                raise RuntimeError
 
-            results = []
-            def loopback(variables: list[str], values: dict[str, bool] = {}) -> None:
-                current_variable = variables[0]
-                for value in [True, False]:
-                    if variables[1:]:
-                        loopback(variables[1:], values | {current_variable: value})
-
-                    else:
-                        results.append([
-                            evaluate(
-                                expression,
-                                variables = values | {current_variable: value},
-                                enable_logging = False
-                            )
-                            for expression in expressions
-                        ])
-
-            loopback(sorted(set(variables)))
             print("\033[36mGiven the following equations:")
             for expression in expressions:
                 print(f"\033[90m  -> {expression}")
 
-            equivalent = all([x == y for x, y in results])
-            consistent = any([x and y for x, y in results])
-            contradictory = inconsistent = all([x != y for x, y in results])
-            inconsistent = not consistent
+            formatted_results = "\033[32mequivalent\033[36m" if result.equivalent else "\033[31mcontradictory\033[36m" if result.contradictory else ""
+            if formatted_results:
+                formatted_results += " and "
 
-            formatted_results = "\033[32mequivalent\033[36m" if equivalent else "\033[31mcontradictory\033[36m" if contradictory else ""
-            if consistent or inconsistent:
-                if formatted_results:
-                    formatted_results += " and "
-
-                formatted_results += "\033[32mconsistent\033[36m" if consistent else "\033[31minconsistent\033[36m" if inconsistent else ""
-
-            print(f"\n\033[36mTheir relationship is {formatted_results}.")
+            print(f"\n\033[36mTheir relationship is {formatted_results}{'\033[32mconsistent\033[36m' if result.consistent else '\033[31minconsistent\033[36m'}.")
             exit()
 
         [run(expression) for expression in expressions]
